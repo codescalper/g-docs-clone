f15abb89e84827b7e3bb5889a205ccc23777715f
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-07-20 09:46:35 +0000

    Realtime changes saved on delta being displayed

--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -1,7 +1,8 @@
-import React, { useCallback, useEffect } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import Quill from "quill";
 import 'quill/dist/quill.snow.css'
 import './style.css'
+import {io} from 'socket.io-client'
 
 const TOOLBAR_OPTIONS = [
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -15,9 +16,49 @@ const TOOLBAR_OPTIONS = [
   ["clean"],
 ]
 
+
+
 export default function TextEditor() {
 
+  const [socket,setSocket] = useState()
+  const [quill,setQuill] = useState()
 
+  useEffect(()=>{
+    const s = io('http://localhost:3001')
+    setSocket(s)
+    return() =>{
+        s.disconnect()
+    } 
+  },[])
+
+
+  useEffect(() =>{
+    if(socket == null || quill == null) return
+    const handler = (delta )=>{
+     quill.updateContents(delta) 
+   }
+    quill.on('revieve-changes',handler)
+  
+    return () =>{
+      quill.off('recieve-changes',handler)
+    }
+  },[socket,quill])
+
+
+
+
+useEffect(() =>{
+  if(socket == null || quill == null) return
+  const handler = (delta,oldDelta,source)=>{
+    if(source !== 'user') return
+    socket.emit('send-changes',delta)
+ }
+  quill.on('text-change',handler)
+
+  return () =>{
+    quill.off('text-changes',handler)
+  }
+})
 
 const wrapperRef = useCallback(wrapper =>{
   if(wrapper ==null ) return 
@@ -25,8 +66,8 @@ const wrapperRef = useCallback(wrapper =>{
   wrapper.innerHTMl = ''
   const editor = document.createElement('div')
   wrapper.append(editor)
-  new Quill(editor,{theme : 'snow' ,modules:{toolbar : TOOLBAR_OPTIONS}})
-  
+  const q = new Quill(editor,{theme : 'snow' ,modules:{toolbar : TOOLBAR_OPTIONS}})
+  setQuill(q)
 
 
 },[])     
