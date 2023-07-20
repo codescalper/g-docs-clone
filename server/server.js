34c906a929d755ceaa2917d52551379d2a42bb85
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-07-20 09:46:11 +0000

    Created socket connection between cilent and server

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const io =require('socket.io')(3001,{
+    cors:{
+        origin:'http://localhost:64773',
+        methods: ['GET','POST']
+    },
+})
+
+io.on('connection',socket=>{
+    socket.on('send-changes',delta =>{
+        socket.broadcast.emit('recieve-changes',delta)
+    })
+ 
+})
