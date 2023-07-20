cbe07261cff55227169a87f56391df8cd35edeb2
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-07-20 09:08:23 +0000

    Added more  toolbar options

--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -3,7 +3,17 @@ import Quill from "quill";
 import 'quill/dist/quill.snow.css'
 import './style.css'
 
-
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ align: [] }],
+  ["image", "blockquote", "code-block"],
+  ["clean"],
+]
 
 export default function TextEditor() {
 
@@ -15,7 +25,8 @@ const wrapperRef = useCallback(wrapper =>{
   wrapper.innerHTMl = ''
   const editor = document.createElement('div')
   wrapper.append(editor)
-  new Quill(editor,{theme : 'snow'})
+  new Quill(editor,{theme : 'snow' ,modules:{toolbar : TOOLBAR_OPTIONS}})
+  
 
 
 },[])     
